47f9cfb7ed7fb29f6d4d0b9cc916c9d9b3ba7f0d
Kosuke Watanabe <kousuke6122@gmail.com>
2025-01-21 18:54:54 +0900

    phpとterrafromを追加

--- dot.config/nvim/lua/config/nvim-treesitter.lua
+++ dot.config/nvim/lua/config/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "bash" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "bash", "terraform", "php" },
     ignore_install = { "sh" },
     sync_install = false,
     highlight = { enable = true },
