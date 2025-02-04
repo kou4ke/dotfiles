15ad68e4a3b4c302d6956e8a30f570e28c6f51c9
Kosuke Watanabe <kousuke6122@gmail.com>
2025-02-04 13:41:44 +0900

    lspconfig側に設定をよせる

--- dot.config/nvim/lua/config/lspconfig.lua
+++ dot.config/nvim/lua/config/lspconfig.lua
@@ -1,36 +1,67 @@
 local lspconfig = require'lspconfig'
 -- cmp_nvim_lspのcapabilitiesを作成
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local schemastore = require('schemastore')
 
 -- LSPサーバーの設定
 -- (ここではpyrightを例とする)
-lspconfig.pyright.setup{}
+lspconfig.pyright.setup{
+    capabilities = capabilities
+}
 -- PHP (Intelephense) の設定
 lspconfig.intelephense.setup{
     capabilities = capabilities
 }
 -- Vue.js (Volar) の設定
 lspconfig.volar.setup{
-    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
+    cmd = { "vls" },
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+    capabilities = capabilities
 }
 -- Terraform (Terraform Language Server) の設定
-lspconfig.terraformls.setup{}
+lspconfig.terraformls.setup{
+    capabilities = capabilities
+}
 -- Shell (Bash Language Server) の設定
-lspconfig.bashls.setup{}
+lspconfig.bashls.setup{
+    capabilities = capabilities
+}
 -- Docker (Dockerfile Language Server) の設定
-lspconfig.dockerls.setup{}
+lspconfig.dockerls.setup{
+    capabilities = capabilities
+}
 -- YAML (YAML Language Server) の設定
-lspconfig.yamlls.setup{}
+lspconfig.yamlls.setup{
+    capabilities = capabilities
+}
 -- JSON (JSON Language Server) の設定
-lspconfig.jsonls.setup{}
+lspconfig.jsonls.setup{
+    cmd = { "vscode-json-languageserver", "--stdio" },
+    capabilities = capabilities,
+    settings = {
+        json = {
+            schemas = schemastore.json.schemas(),
+            validate = { enable = true }
+        },
+    },
+}
 -- HTML (HTML Language Server) の設定
-lspconfig.html.setup{}
+lspconfig.html.setup{
+    capabilities = capabilities
+}
 -- CSS (CSS Language Server) の設定
-lspconfig.cssls.setup{}
+lspconfig.cssls.setup{
+    capabilities = capabilities
+}
 -- SCSS (Sass Language Server) の設定
-lspconfig.sqlls.setup{}
+lspconfig.sqlls.setup{
+    capabilities = capabilities
+}
 -- SQL (SQL Language Server) の設定
-lspconfig.sqls.setup{}
+lspconfig.sqls.setup{
+    capabilities = capabilities
+}
 -- lua (lua Language Server) の設定
 lspconfig.lua_ls.setup{
     settings = {
