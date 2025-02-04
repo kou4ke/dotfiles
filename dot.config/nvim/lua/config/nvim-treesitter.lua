a8e7c50818752c29bff5a702850be6458a545e76
Kosuke Watanabe <kousuke6122@gmail.com>
2025-02-04 13:42:08 +0900

    記述をよみやすく整形

--- dot.config/nvim/lua/config/nvim-treesitter.lua
+++ dot.config/nvim/lua/config/nvim-treesitter.lua
@@ -1,5 +1,21 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "bash", "terraform", "php" },
+    ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "html",
+        "bash",
+        "vue",
+        "json",
+        "yaml",
+        "terraform",
+        "php"
+    },
     ignore_install = { "sh" },
     sync_install = false,
     highlight = { enable = true },
