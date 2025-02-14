2b2e034e8b8692f7021ba960d07197e7b81f51e6
Kosuke Watanabe <kousuke6122@gmail.com>
2025-02-14 12:52:04 +0900

    E117エラーの一旦の回避で幅を大きくする

--- dot.config/nvim/lua/config/neo-tree.lua
+++ dot.config/nvim/lua/config/neo-tree.lua
@@ -1,9 +1,13 @@
 -- プレフィックスを変数として定義
 local neotree_prefix = ",t"  -- または好みのプレフィックス
 
-vim.keymap.set('n', neotree_prefix .. 't', ':Neotree filesystem reveal left<CR>')
+vim.keymap.set('n', neotree_prefix .. 'f', ':Neotree filesystem reveal left<CR>')
+vim.keymap.set('n', neotree_prefix .. 't', ':Neotree toggle<CR>')
+
+local neo_tree_width = 70
 
 require("neo-tree").setup({
+
   use_nerdfonts = true,
   default_component_configs = {
     icon = {
@@ -40,7 +44,7 @@ require("neo-tree").setup({
   },
   filesystem = {
     window = {
-      width = 30  -- ここで幅を設定します。数値は任意で変更可能です。
+      width = neo_tree_width  -- ここで幅を設定します。数値は任意で変更可能です。
     },
     use_libuv_file_watcher = false,
     follow_current_file = {
