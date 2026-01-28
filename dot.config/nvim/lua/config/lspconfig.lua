7176f7f9ec738457d4a7deed1980fe6373323e86
Kosuke Watanabe <kousuke6122@gmail.com>
2026-01-28 11:53:48 +0900

    nvim v0.11にあわせた記述に修正

--- dot.config/nvim/lua/config/lspconfig.lua
+++ dot.config/nvim/lua/config/lspconfig.lua
@@ -1,17 +1,20 @@
-local lspconfig = require'lspconfig'
 -- cmp_nvim_lspのcapabilitiesを作成
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local schemastore = require('schemastore')
 local lsp_diagnostic = require("config.lsp-diagnostic")
 
+-- グローバル設定 (全LSPサーバーに適用)
+vim.lsp.config('*', {
+    capabilities = capabilities,
+    on_attach = lsp_diagnostic.on_attach,
+})
+
 -- LSPサーバーの設定
--- (ここではpyrightを例とする)
-lspconfig.pyright.setup{
-    capabilities = capabilities
-}
+-- Python (Pyright) の設定
+vim.lsp.config('pyright', {})
+
 -- PHP (Intelephense) の設定
-lspconfig.intelephense.setup{
+vim.lsp.config('intelephense', {
     cmd = { "intelephense", "--stdio" },
     filetypes = { "php" },
     settings = {
@@ -26,51 +29,41 @@ lspconfig.intelephense.setup{
             stubs = { "laravel", "eloquent", "blade" }
         }
     },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
+
 -- volar (Volar Language Server) の設定
-lspconfig.volar.setup{
+vim.lsp.config('volar', {
     cmd = { "vls" },
     filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
+
 -- Terraform (Terraform Language Server) の設定
-lspconfig.terraformls.setup{
+vim.lsp.config('terraformls', {
     filetypes = { 'terraform' },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
+
 -- Shell (Bash Language Server) の設定
-lspconfig.bashls.setup{
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+vim.lsp.config('bashls', {})
+
 -- Docker (Dockerfile Language Server) の設定
-lspconfig.dockerls.setup{
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+vim.lsp.config('dockerls', {})
+
 -- docker-compose (Docker Compose Language Server) の設定
-lspconfig.docker_compose_language_service.setup{
-   cmd = { "docker-compose-language-service", "--stdio" },
-   filetypes = { "docker-compose" },
-   capabilities = capabilities,
-   on_attach = lsp_diagnostic.on_attach,
-   settings = {
-     dockerCompose = {
-       filetypes = { "docker-compose" },
-     },
-   },
- }
+vim.lsp.config('docker_compose_language_service', {
+    cmd = { "docker-compose-language-service", "--stdio" },
+    filetypes = { "docker-compose" },
+    settings = {
+        dockerCompose = {
+            filetypes = { "docker-compose" },
+        },
+    },
+})
+
 -- YAML (YAML Language Server) の設定
-lspconfig.yamlls.setup{
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+vim.lsp.config('yamlls', {})
+
 -- JSON (JSON Language Server) の設定
-lspconfig.jsonls.setup{
+vim.lsp.config('jsonls', {
     cmd = { "vscode-json-languageserver", "--stdio" },
     settings = {
         json = {
@@ -78,76 +71,66 @@ lspconfig.jsonls.setup{
             validate = { enable = true }
         },
     },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
+
 -- HTML (HTML Language Server) の設定
-lspconfig.html.setup{
+vim.lsp.config('html', {
     filetypes = { "html" },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
+
 -- CSS (CSS Language Server) の設定
-lspconfig.cssls.setup{
+vim.lsp.config('cssls', {
     filetypes = { "css" },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
--- SCSS (Sass Language Server) の設定
---
-lspconfig.tailwindcss.setup{
+})
+
+-- Tailwind CSS の設定
+vim.lsp.config('tailwindcss', {
     cmd = { "npx", "tailwindcss-language-server", "--stdio" },
     filetypes = { "html", "css", "scss", "sass", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
--- SQL (SQL Language Server) の設定
--- lspconfig.sqls.setup{
---     cmd = { "sqls" },
---     filetypes = { "sql" },
---     settings = {
---       -- sqls = {
---       --   connections = {
---       --     {
---       --       driver = 'mysql',
---       --       dataSourceName = 'user:password@tcp(127.0.0.1:3306)/dbname',
---       --     },
---       --   },
---       -- },
---     },
---     capabilities = capabilities
--- }
+})
+
 -- Vim (Vim Language Server) の設定
-lspconfig.vimls.setup{
+vim.lsp.config('vimls', {
     cmd = { "vim-language-server", "--stdio" },
     filetypes = { "vim" },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
 
 -- lua (lua Language Server) の設定
-lspconfig.lua_ls.setup{
+vim.lsp.config('lua_ls', {
     filetypes = { "lua" },
     settings = {
         Lua = {
             runtime = {
-              -- Luaのバージョン（通常はLuaJITを使用）
-              version = 'LuaJIT',
+                version = 'LuaJIT',
             },
             diagnostics = {
-              -- グローバル変数の認識を追加する
-              globals = { 'vim' },
+                globals = { 'vim' },
             },
             workspace = {
-              -- LuaランタイムファイルをLSPに通知
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
             },
             telemetry = {
-              enable = false,
+                enable = false,
             },
         },
     },
-    capabilities = capabilities,
-    on_attach = lsp_diagnostic.on_attach,
-}
+})
+
+-- LSPサーバーを有効化
+vim.lsp.enable({
+    'pyright',
+    'intelephense',
+    'volar',
+    'terraformls',
+    'bashls',
+    'dockerls',
+    'docker_compose_language_service',
+    'yamlls',
+    'jsonls',
+    'html',
+    'cssls',
+    'tailwindcss',
+    'vimls',
+    'lua_ls',
+})
